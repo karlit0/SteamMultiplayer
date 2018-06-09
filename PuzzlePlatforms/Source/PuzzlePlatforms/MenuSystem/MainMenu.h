cfe961e0b74c280cdb8351fae4b045d3164e0c00
DESKTOP-LLDKI5Q\Karlito <astipanovic23@gmail.com>
2018-06-09 18:35:29 +0200

    Initial commit

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MenuWidget.h"
+#include "MainMenu.generated.h"
+
+class UButton;
+class UWidgetSwitcher;
+class UEditableTextBox;
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UMainMenu : public UMenuWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual bool Initialize() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	UButton* HostButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* JoinButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* QuitButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* CancelJoinMenuButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ConfirmJoinMenuButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UWidgetSwitcher* MenuSwitcher;
+
+	UPROPERTY(meta = (BindWidget))
+	UWidget* MainMenu;
+
+	UPROPERTY(meta = (BindWidget))
+	UWidget* JoinMenu;
+
+	UPROPERTY(meta = (BindWidget))
+	UEditableTextBox* IPAddressField;
+
+	UFUNCTION()
+	void HostServer();
+
+	UFUNCTION()
+	void JoinServer();
+
+	UFUNCTION()
+	void OpenJoinMenu();
+
+	UFUNCTION()
+	void OpenMainMenu();
+	
+	UFUNCTION()
+	void QuitPressed();
+};
