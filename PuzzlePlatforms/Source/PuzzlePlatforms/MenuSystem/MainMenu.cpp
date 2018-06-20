8fe6f0c66feca05f87e7bfd5a72e3d582495a8b4
DESKTOP-LLDKI5Q\Karlito <astipanovic23@gmail.com>
2018-06-21 00:11:00 +0200

    62. Displaying Search Result Properties

--- PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
@@ -21,7 +21,7 @@ UMainMenu::UMainMenu(const FObjectInitializer& ObjectInitializer)
 	ServerRowClass = ServerRowBPClass.Class;
 }
 
-void UMainMenu::SetServerList(TArray<FString> ServerNames)
+void UMainMenu::SetServerList(TArray<FServerData> ServerNames)
 {
 	UWorld* World = GetWorld();
 	if (!ensure(World != nullptr))
@@ -30,13 +30,16 @@ void UMainMenu::SetServerList(TArray<FString> ServerNames)
 	ServerList->ClearChildren();
 
 	uint32 i = 0;
-	for (const FString& ServerName : ServerNames)
+	for (const FServerData& ServerData : ServerNames)
 	{
 		UServerRow* Row = CreateWidget<UServerRow>(World, ServerRowClass);
 		if (!ensure(Row != nullptr))
 			return;
 
-		Row->ServerName->SetText(FText::FromString(ServerName));
+		Row->ServerName->SetText(FText::FromString(ServerData.Name));
+		Row->HostUser->SetText(FText::FromString(ServerData.HostUserName));
+		FString FractionText = FString::Printf(TEXT("%d/%d"), ServerData.CurrentPlayers, ServerData.MaxPlayers);
+		Row->ConnectionFraction->SetText(FText::FromString(FractionText));
 		Row->Setup(this, i);
 		++i;
 
